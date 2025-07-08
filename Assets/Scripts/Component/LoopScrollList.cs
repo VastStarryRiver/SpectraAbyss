518b5e152327a7f9a1292b04175cef0c8bdb8553
回溯 <1214627679goddragon@gmail.com>
2025-07-08 17:28:56 +0800

    更新日志
    更新

--- Assets/Scripts/Component/LoopScrollList.cs
+++ Assets/Scripts/Component/LoopScrollList.cs
@@ -1,120 +1,234 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
 
-
-
-namespace Invariable
+public class LoopScrollList : ScrollRect
 {
-    public class LoopScrollList : MonoBehaviour
+    public RectTransform m_itemPrefab;
+    private int m_type = 0;
+    private int m_totalCount = 0;
+    private Action<int, RectTransform> m_updateFunc;
+    private Action<Vector2> m_onValueChangedFunc;
+    private int m_index1 = -1;
+    private int m_index2 = -1;
+
+
+
+    protected override void Awake()
     {
-        private RectTransform content;
-        private GameObject itemPrefab;
-        private int totalCount = 0; // 总项数
-        private int showCount = 0; // 可见项数
-        private List<RectTransform> tranItems;
-        private float itemHeight;
-        private int currItemIndex = 0;
-        private bool isInit = false;
+        base.Awake();
 
+        content = transform.Find("Viewport/Content") as RectTransform;
+        viewport = transform.Find("Viewport") as RectTransform;
 
-
-        private void Update()
+        onValueChanged.AddListener((pos) =>
         {
-            if (!isInit)
+            if (m_type == 1)
             {
-                return;
-            }
-
-            if (content.anchoredPosition.y < currItemIndex * itemHeight)
-            {
-                ScrollUp();
-            }
-            else if (content.anchoredPosition.y > (currItemIndex + 1) * itemHeight)
-            {
-                ScrollDown();
-            }
-        }
-
-
-
-        public void Init(int totalCount, int showCount)
-        {
-            if (isInit)
-            {
-                return;
-            }
-
-            this.totalCount = totalCount;
-            this.showCount = showCount;
-
-            itemHeight = itemPrefab.GetComponent<RectTransform>().rect.height;
-            tranItems = new List<RectTransform>();
-
-            UpdateAllItems();
-
-            content.sizeDelta = new Vector2(content.sizeDelta.x, itemHeight * totalCount);
-
-            isInit = true;
-        }
-
-        private void ScrollUp()
-        {
-            currItemIndex--;
-
-            if (currItemIndex < 0)
-            {
-                currItemIndex = totalCount - 1;
-            }
-
-            int itemIndex = 0;
-
-            if (currItemIndex > 0)
-            {
-                itemIndex = currItemIndex % totalCount;
-            }
-
-            UpdateItem(itemIndex, currItemIndex + 1);
-        }
-
-        private void ScrollDown()
-        {
-            currItemIndex++;
-
-            if (currItemIndex >= totalCount)
-            {
-                currItemIndex = 0;
-            }
-
-            int itemIndex = 0;
-
-            if (currItemIndex > 0)
-            {
-                itemIndex = currItemIndex % totalCount;
-            }
-
-            UpdateItem(itemIndex, currItemIndex + 1);
-        }
-
-        private void UpdateAllItems()
-        {
-            for (int i = 0; i < showCount; i++)
-            {
-                if (tranItems[i] == null)
+                if (content.sizeDelta.x <= viewport.sizeDelta.x)
                 {
-                    tranItems.Add(Instantiate(itemPrefab, content).GetComponent<RectTransform>());
+                    return;
                 }
 
-                int dataIndex = (currItemIndex + i) % totalCount + 1;
-
-                UpdateItem(i, dataIndex);
+                UpdateHorizonalItem(pos);
             }
+            else if (m_type == 2)
+            {
+                if (content.sizeDelta.y <= viewport.sizeDelta.y)
+                {
+                    return;
+                }
+
+                UpdateVerticalItem(pos);
+            }
+            else
+            {
+                return;
+            }
+
+            m_onValueChangedFunc?.Invoke(pos);
+        });
+    }
+
+
+
+    public void AddOnValueChangedListener(Action<Vector2> callBack)
+    {
+        m_onValueChangedFunc = callBack;
+    }
+
+    /// <summary>
+    /// 初始化循环列表
+    /// </summary>
+    /// <param name="type">1横向 2纵向</param>
+    /// <param name="showCount">需要比可见区域的元素多两个</param>
+    /// <param name="totalCount">总元素个数</param>
+    /// <param name="updateFunc">刷新函数</param>
+    /// <param name="callBack">回调函数</param>
+    public void Init(int type, int showCount, int totalCount, Action<int, RectTransform> updateFunc, Action callBack = null)
+    {
+        m_type = type;
+        m_updateFunc = updateFunc;
+        m_totalCount = totalCount;
+
+        for (int i = 0; i < content.childCount; i++)
+        {
+            var item = content.GetChild(i);
+            item.gameObject.SetActive(false);
         }
 
-        private void UpdateItem(int itemIndex, int dataIndex)
+        for (int i = 0; i < showCount; i++)
         {
-            tranItems[itemIndex].anchoredPosition = new Vector2(0, -1 * (dataIndex - 1) * itemHeight);
-            tranItems[itemIndex].GetComponentInChildren<Text>().text = "Item " + dataIndex;
+            Transform item = content.GetChild(i);
+
+            if (item == null)
+            {
+                item = Instantiate(m_itemPrefab.gameObject, content).transform;
+            }
+
+            item.name = "Ts_Item" + i;
+
+            m_updateFunc.Invoke(i, (RectTransform)item);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item);
+        }
+
+        if (m_type == 1)
+        {
+            HorizontalLayoutGroup horizontalLayoutGroup = content.GetComponent<HorizontalLayoutGroup>();
+            horizontalLayoutGroup.spacing = 0;
+            content.sizeDelta = new Vector2(m_itemPrefab.sizeDelta.x * m_totalCount, content.sizeDelta.y);
+        }
+        else if (m_type == 2)
+        {
+            VerticalLayoutGroup verticalLayoutGroup = content.GetComponent<VerticalLayoutGroup>();
+            verticalLayoutGroup.spacing = 0;
+            content.sizeDelta = new Vector2(content.sizeDelta.x, m_itemPrefab.sizeDelta.y * m_totalCount);
+        }
+
+        content.anchoredPosition = Vector2.zero;
+
+        LayoutRebuilder.ForceRebuildLayoutImmediate(content);
+
+        m_index1 = -1;
+        m_index2 = -1;
+
+        callBack?.Invoke();
+    }
+
+    private void UpdateHorizonalItem(Vector2 pos)
+    {
+        float offset = viewport.sizeDelta.x - content.sizeDelta.x;
+        float min = offset * pos.x;
+        float max = min + viewport.sizeDelta.x;
+
+        for (int i = 1; i < m_totalCount - 1; i++)
+        {
+            RectTransform item1 = content.GetChild(i) as RectTransform;
+            float min2 = item1.sizeDelta.x * i;
+            float max2 = min2 + item1.sizeDelta.x;
+
+            if (min2 >= min && min2 <= min + 0.1f)
+            {
+                if (m_index1 != i)
+                {
+                    m_index1 = i;
+
+                    Transform item2 = content.GetChild(content.childCount - 1);
+
+                    item2.SetSiblingIndex(0);
+
+                    item2.name = "Ts_Item" + i;
+
+                    m_updateFunc.Invoke(i, (RectTransform)item2);
+
+                    LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item2);
+                }
+            }
+            else if (max2 <= max && max2 >= max - 0.1f)
+            {
+                if (m_index2 != i)
+                {
+                    m_index2 = i;
+
+                    Transform item2 = content.GetChild(0);
+
+                    item2.SetSiblingIndex(content.childCount - 1);
+
+                    item2.name = "Ts_Item" + i;
+
+                    m_updateFunc.Invoke(i, (RectTransform)item2);
+
+                    LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item2);
+                }
+
+                break;
+            }
+        }
+    }
+
+    private void UpdateVerticalItem(Vector2 pos)
+    {
+        float offset = viewport.sizeDelta.y - content.sizeDelta.y;
+        float min = offset * pos.y;
+        float max = min + viewport.sizeDelta.y;
+
+        for (int i = 1; i < m_totalCount - 1; i++)
+        {
+            RectTransform item1 = content.GetChild(i) as RectTransform;
+            float min2 = item1.sizeDelta.y * i;
+            float max2 = min2 + item1.sizeDelta.y;
+
+            if (min2 >= min && min2 <= min + 0.1f)
+            {
+                if (m_index1 != i)
+                {
+                    m_index1 = i;
+
+                    Transform item2 = content.GetChild(content.childCount - 1);
+
+                    item2.SetSiblingIndex(0);
+
+                    item2.name = "Ts_Item" + i;
+
+                    m_updateFunc.Invoke(i, (RectTransform)item2);
+
+                    LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item2);
+                }
+            }
+            else if (max2 <= max && max2 >= max - 0.1f)
+            {
+                if (m_index2 != i)
+                {
+                    m_index2 = i;
+
+                    Transform item2 = content.GetChild(0);
+
+                    item2.SetSiblingIndex(content.childCount - 1);
+
+                    item2.name = "Ts_Item" + i;
+
+                    m_updateFunc.Invoke(i, (RectTransform)item2);
+
+                    LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item2);
+                }
+
+                break;
+            }
+        }
+    }
+
+    public void UpdateAllItem()
+    {
+        for (int i = 0; i < content.childCount; i++)
+        {
+            Transform item = content.GetChild(i);
+
+            int index = int.Parse(item.name.Replace("Ts_Item", ""));
+
+            m_updateFunc.Invoke(index, (RectTransform)item);
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)item);
         }
     }
 }
